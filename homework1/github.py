8a98d0672969b0f9650c442824f2250a55cc3d61
16684252 <Belov.A.Andr@sberbank.ru>
2020-06-03 09:13:52 +0300

    Initial commit

---
+++ homework1/github.py
@@ -0,0 +1,20 @@
+import requests
+
+test_token = '2e70d2261f73ab84186c74fc2c854443f8153716'
+
+usernames = ['iGhibli']
+
+params = {
+    'type': 'owner'
+}
+
+headers = {
+    'Authorization': f'token {test_token}',
+}
+
+for username in usernames:
+    url_address = f'https://api.github.com/users/{username}/repos'
+    response = requests.get(url_address, params=params)
+
+    with open(f'{username}.json', 'w') as f:
+        f.write(response.text)
